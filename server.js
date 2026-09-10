534e6385df932ab7e0b7003fb6b43f21c33a5768
Rahma Yahia <rahmahamam521@gmail.com>
2026-09-10 22:30:14 +0300

    inital setup , database connection

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+});
